b6e47c083af0a6e4bcaa62d5fff7fccd02a4230d
Ivan <cherniv@gmail.com>
2017-10-21 19:22:04 +0300

    cleaning

--- index.js
+++ index.js
@@ -5,20 +5,18 @@ import { Image } from 'react-native';
 import * as flags from './flags';
 
 type Props = {
-  size: 16 | 24 | 32 | 48 | 64,
   code: string,
-  type?: 'flat' | 'shiny',
   style?: any,
 };
 
-const Flag = ({ size = 64, code, type = 'shiny', style }: Props) => {
-  const flag = flags[type][`icons${size}`][code];
-  const unknownFlag = flags[type][`icons${size}`]['unknown'];
+const Flag = ({ code, style }: Props) => {
+  const flag = flags['flat'][`icons16`][code];
+  const unknownFlag = flags['flat'][`icons16`]['unknown'];
 
   return (
     <Image
       source={flag || unknownFlag}
-      style={[{ width: size, height: size }, style]}
+      style={style}
     />
   );
 };
